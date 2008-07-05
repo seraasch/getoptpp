1adf22eefe670e7bbb5d5faca7bb2f83450c604c
danielgutson <danielgutson@localhost>
2008-07-05 18:15:10 +0000

    Version 2.12: changed license from Affero to non-Affero.

--- example_general1.cpp
+++ example_general1.cpp
@@ -1,20 +1,20 @@
 /*
 GetOpt_pp:	Yet another C++ version of getopt.
-    Copyright (C) 2007  Daniel Gutson, FuDePAN
-
-    This file is part of GetOpt_pp.
+    Copyright (C) 2007, 2008  Daniel Gutson, FuDePAN
     
-    GetOpt_pp is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+    This file is part of GetOpt_pp.
 
-    This program is distributed in the hope that it will be useful,
+    GetOpt_pp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    board-games is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+    GNU General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
+    You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     
