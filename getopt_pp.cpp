2809efcbcb6b188d22f9a2e6f3bb5073a179ff83
danielgutson <danielgutson@localhost>
2010-07-01 00:20:54 +0000

    Fixed license heading.

--- getopt_pp.cpp
+++ getopt_pp.cpp
@@ -9,7 +9,7 @@ GetOpt_pp:	Yet another C++ version of getopt.
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    board-games is distributed in the hope that it will be useful,
+    GetOpt_pp is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
